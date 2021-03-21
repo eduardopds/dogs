0944852403a61c7babce33942bfdab6a9b14a7e0
eduardopds <eduardo.pereira.santos@ccc.ufcg.edu.br>
2021-03-21 12:06:09 -0300

    estrutura inicial

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+
+import { Footer } from './Components/Footer';
+import { Header } from './Components/Header';
+import { Home } from './Components/Home';
+import { Login } from './Components/Login/Login';
+
 function App() {
-  return <div>App React</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;

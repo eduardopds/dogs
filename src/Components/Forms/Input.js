c7cff7b8bc70cdcd7bf035df170b12cd92e14d0d
eduardopds <eduardo.pereira.santos@ccc.ufcg.edu.br>
2021-03-30 13:33:19 -0300

    Adicionando custom hook

--- src/Components/Forms/Input.js
+++ src/Components/Forms/Input.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import styles from './Input.module.css'
 
-export const Input = ({ label , type, name }) => {
+export const Input = ({ label , type, name, value, onChange,error,onBlur}) => {
   return (
     <div className={styles.wrapper}>
       <label className={styles.label} htmlFor={name}>{label}</label>
-      <input id={name} name={name} className={styles.input} type={type} />
-      <p className={styles.error}>Error</p>
+      <input 
+        id={name}
+        name={name} 
+        className={styles.input} 
+        type={type}
+        value={value} 
+        onChange={onChange}
+        onBlur={onBlur}
+      />
+      {error && <p className={styles.error}>{error}</p>}
     </div>);
 };
